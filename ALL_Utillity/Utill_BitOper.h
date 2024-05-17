68669453a0ba3a6bf024292d9529bfdae63cc785
DrumTimes <nohc2001@naver.com>
2024-05-17 14:21:54 +0000

    save

--- ALL_Utillity/Utill_BitOper.h
+++ ALL_Utillity/Utill_BitOper.h
@@ -17,9 +17,12 @@ p : pass
 */
 
 // basic parameter type is unsigned integer types.
-// control last bit
+// 2-1 control last bit
+
+// l1to0(x) == 0 -> x == 2^(any_n)
 #define l1to0(x) (x & (x-1))
 #define l0to1(x) (x | (x+1))
+// en1to0(x) == 0 -> x == 2^(any_n) - 1
 #define en1to0(x) (x & (x+1))
 #define en0to1(x) (x | (x-1))
 #define l0to1_rest0(x) (~x & (x+1))
@@ -29,6 +32,9 @@ p : pass
 #define l1p_rest0(x) (x & (-x))
 #define l1p_en0to1_rest0(x) (x ^ (x-1))
 #define l0to1_en1p_rest0(x) (x ^ (x+1))
+// ln1to0(x) == 0 -> x == 2^(any_n) - 2^(any_m) {n > m >= 0}
 #define ln1to0(x) (((x | (x-1))+1) & x) // or (((x & -x)+x)&x)
+
+//extend De Morgan's Law
 
 #endif
