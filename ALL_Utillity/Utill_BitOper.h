2a25e80a754f3ca66b78807290c05e6e200b24d5
DrumTimes <nohc2001@naver.com>
2024-05-18 14:18:41 +0000

    save

--- ALL_Utillity/Utill_BitOper.h
+++ ALL_Utillity/Utill_BitOper.h
@@ -3,6 +3,12 @@
 #ifndef UTILL_BITOPER
 #define UTILL_BITOPER
 
+//###################################################################################
+//###################################################################################
+// 2. Basic Knowledge
+//###################################################################################
+//###################################################################################
+
 /*
 symbol guide
 R : right
@@ -17,7 +23,10 @@ p : pass
 */
 
 // basic parameter type is unsigned integer types.
+
+//--------------------------------------------------------------
 // 2-1 control last bit
+//--------------------------------------------------------------
 
 // l1to0(x) == 0 -> x == 2^(any_n)
 #define l1to0(x) (x & (x-1))
@@ -47,4 +56,151 @@ p : pass
 ~(x+y) == ~x-y
 ~(x-y) == ~x+y
 */
+
+//get minimum return that bigger than x and 1bit count same with x.
+// form : ((x ^ (x + (x & -x)) >> 2) / (x & -x)) | (x + (x & -x))
+unsigned int Gosper175(unsigned int x){
+    unsigned int s, r, o;
+    s = l1p_rest0(x);
+    r = x + s;
+    o = x ^ r;
+    o = (o >> 2) / s;
+    return r | o;
+}
+
+//--------------------------------------------------------------
+// 2-2 addition combine with logical operation
+//--------------------------------------------------------------
+
+// add operation and logical operation identity equation
+/*
+-x == ~x + 1 == ~(x - 1)
+~x == -x-1
+-~x == x+1
+~-x == x-1
+x + y == x - ~y - 1 == (x ^ y) + 2 * (x & y) == (x | y) + (x & y) == 2 * (x | y) - (x ^ y)
+x - y == x + ~y + 1 == (x ^ y) + 2 * (~x & y) == (x & ~y) - (~x & y) == 2 * (x & ~y) - (x ^ y)
+x ^ y == (x | y) - (x & y)
+x & ~y == (x | y) - y == x - (x & y)
+~(x - y) == y - x - 1 == ~x + y
+x≡y == (x & y) - (x | y) - 1 == (x & y) - ~(x | y)
+x | y == (x & ~y) + y
+x & y == (~x | y) - ~x
+*/
+
+//--------------------------------------------------------------
+// 2-3 logical and arithmetic inequation
+//--------------------------------------------------------------
+
+//16 logical operation with every xy cases
+//every bit location is seperate.
+/*
+        (0, 0)  (1, 0)  (0, 1)  (1, 1)
+0       0       0       0       0
+x & y   0       0       0       1
+x & ~y  0       1       0       0
+x       0       1       0       1
+~x & y  0       0       1       0
+y       0       0       1       1
+x ^ y   0       1       1       0
+x | y   0       1       1       1
+~(x | y)1       0       0       0
+x≡y    1       0       0       1
+~y      1       1       0       0
+x | ~y  1       1       0       1
+~x      1       0       1       0
+~x | y  1       0       1       1
+~(x & y)1       1       1       0
+1       1       1       1       1
+
+
+sort by siz >> good to find inequation
+        (0, 0)  (1, 0)  (0, 1)  (1, 1)
+0       0       0       0       0
+
+~(x | y)1       0       0       0
+x & ~y  0       1       0       0
+~x & y  0       0       1       0
+x & y   0       0       0       1
+
+~y      1       1       0       0
+~x      1       0       1       0
+x≡y    1       0       0       1
+x ^ y   0       1       1       0
+x       0       1       0       1
+y       0       0       1       1
+
+~(x & y)1       1       1       0
+x | ~y  1       1       0       1
+~x | y  1       0       1       1
+x | y   0       1       1       1
+
+1       1       1       1       1
+
+<2 > 1> inequation
+~y ≥ ~(x | y)
+~y ≥ x & ~y
+~x ≥ ~(x | y)
+~x ≥ ~x & y
+x≡y ≥ ~(x | y)
+x≡y ≥ x & y
+x ^ y ≥ x & ~y
+x ^ y ≥ ~x & y
+x ≥ x & ~y
+x ≥ x & y
+y ≥ ~x & y
+y ≥ x & y
+
+<3 > 2> inequation
+~(x & y) ≥ ~y
+~(x & y) ≥ ~x
+~(x & y) ≥ x ^ y
+x | ~y ≥ ~y
+x | ~y ≥ x≡y
+x | ~y ≥ x
+~x | y ≥ ~x
+~x | y ≥ x≡y
+~x | y ≥ y
+x | y ≥ x ^ y
+x | y ≥ x
+x | y ≥ y
+*/
+
+//mix logical and arithmetic expression inequation
+/*
+(x | y) ≥ max(x, y)
+min(x, y) ≥ (x & y)
+if(carry == 0) { x + y ≥ (x | y) }
+if(carry == 1) { (x | y) > x + y}
+x ^ y ≥ abs(x - y)
+*/
+
+//--------------------------------------------------------------
+// 2-4 absolute function
+//--------------------------------------------------------------
+/*
+y = x >> 31 
+// 4byte = 32bit 32-1 = 31 (2^n-1)
+abs(x) == (x ^ y) - y == (x + y) ^ y == x - (2*x & y)
+if(cpu's multiply ±1 operation is faster than normal) abs(x) == ((x >> 30) | 1) * x 
+nabs(x) == y - (x ^ y) == (y - x) ^ y == (2*x & y) - x
+*/
+
+#define abs(x) (x - ((x << 1) & (x >> 31)))
+#define nabs(x) (((x << 1) & (x >> 31)) - x)
+
+//--------------------------------------------------------------
+// 2-5 average of two integer
+//--------------------------------------------------------------
+/*
+unsigned integer floor average (never occur overflow)
+(x & y) + ((x ^ y) >> 1)
+unsigned integer ceiling average (never occur overflow)
+(x | y) - ((x ^ y) >> 1)
+signed integer 
+*/
+
+//--------------------------------------------------------------
+// 2-5 sign extension
+//--------------------------------------------------------------
 #endif
