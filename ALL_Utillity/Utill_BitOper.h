0ee1932f43967b2056c6e7e72147f10f6821cf88
DrumTimes <nohc2001@naver.com>
2024-05-19 14:33:52 +0000

    save

--- ALL_Utillity/Utill_BitOper.h
+++ ALL_Utillity/Utill_BitOper.h
@@ -186,8 +186,8 @@ if(cpu's multiply ±1 operation is faster than normal) abs(x) == ((x >> 30) | 1)
 nabs(x) == y - (x ^ y) == (y - x) ^ y == (2*x & y) - x
 */
 
-#define abs(x) (x - ((x << 1) & (x >> 31)))
-#define nabs(x) (((x << 1) & (x >> 31)) - x)
+#define abs_int(x) (x - ((x << 1) & (x >> 31)))
+#define nabs_int(x) (((x << 1) & (x >> 31)) - x)
 
 //--------------------------------------------------------------
 // 2-5 average of two integer
@@ -205,7 +205,7 @@ In C language, just change type "unsigned int" to "int".
 #define ceiling_average(x, y) (x | y) - ((x ^ y) >> 1)
 
 //--------------------------------------------------------------
-// 2-5 sign extension
+// 2-6 sign extension
 //--------------------------------------------------------------
 
 /*
@@ -218,7 +218,7 @@ how to operate ?
 #define sign_extension_int(x) ((x + 0x00000080) & 0x000000FF) - 0x00000080
 
 //--------------------------------------------------------------
-// 2-6 signed right shift
+// 2-7 signed right shift
 //--------------------------------------------------------------
 
 /*
@@ -229,7 +229,7 @@ t = 0x80000000 >> n; ((x >> n) ^ t) - t
 */
 
 //--------------------------------------------------------------
-// 2-7 sign function
+// 2-8 sign function
 //--------------------------------------------------------------
 
 /*
@@ -240,11 +240,43 @@ sign(x) = if(x < 0) {-1} else if(x > 0) {1} else {0}
 //if signed right shift instruct is not exist, sign(x) = -((unsigned int)x >> 31) | ((unsigned int)-x >> 31)
 
 //--------------------------------------------------------------
-// 2-8 compare function
+// 2-9 compare function
 //--------------------------------------------------------------
 
 /*
 cmp(x, y) = if(x < y) {-1} else if(x > y) {1} else {0}
 */
 #define cmp_int(x, y) ((x > y) - (x < y))
+
+//--------------------------------------------------------------
+// 2-10 sign sending function
+//--------------------------------------------------------------
+
+/*
+ISIGN(x, y) == if(y >= 0) abs(x) else nabs(x)
+*/
+
+inline int isign_int(int x, int y){
+        int t = y >> 31;
+        return (abs_int(x) ^ t) - t;
+}
+
+//--------------------------------------------------------------
+// 2-11 decoding 0 to max value + 1 (2^n) (in n bit)
+//--------------------------------------------------------------
+
+/*
+ex> 4bit data decode to 5bit data
+00000 -> 10000
+00000 - 1 = 11111
+11111 & 01111 = 01111
+01111 + 1 = 10000
+*/
+
+#define decoding_0toMaxPlus1_uint_to_ulonglong(x) (((unsigned long long)x-1) & (unsigned long long)(unsigned int)(-1)) + 1
+// just casting to uint to encode back.
+
+//--------------------------------------------------------------
+// 2-12 comparison predicate
+//--------------------------------------------------------------
 #endif
